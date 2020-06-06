0b8ae47f2e9e19c5757d023fb9e042f9688d13bc
oudehomar <oudeh92@hotmail.com>
2020-06-06 16:31:17 +0200

    deleteTodo and updateTodo added

--- todo/src/main/java/todo/controller/TodoController.java
+++ todo/src/main/java/todo/controller/TodoController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +42,35 @@ public class TodoController {
 	}
 	
 	
+	
+	
+	@DeleteMapping("delete/{id}")
+	public ResponseEntity<?> deleteTodo(@PathVariable Integer id){
+		
+		if(this.todoRepository.findById(id).isPresent()) {
+			this.todoRepository.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.OK);
+			
+		}else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		
+		
+	}
+	
+	@PutMapping("update/{id}")
+	public ResponseEntity<?> updateTodo(@PathVariable Integer id, @RequestBody Todo todo ){
+		
+		if(this.todoRepository.findById(id).isPresent()) {
+			
+			todo.setId(id);
+			return new ResponseEntity<>(this.todoRepository.save(todo),HttpStatus.OK);
+		}
+		else {
+			
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
 	
 	
 
