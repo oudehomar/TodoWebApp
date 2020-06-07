a68daa0667e0f08f0676d9ba67afdbb6ac503ad6
oudehomar <oudeh92@hotmail.com>
2020-06-07 17:10:33 +0200

    angular project created

---
+++ todo/src/main/java/todo/WebConfig.java
@@ -0,0 +1,18 @@
+package todo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry corsRegistry) {
+
+		corsRegistry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST",
+				"PUT", "DELETE");
+
+	}
+
+}
